d4b3e15217c0cede2ea6521bf2617913e228a361
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-03-21 15:18:32 -0400

    implement whitebox tests

--- src/test/java/com/ontariotechu/sofe3980U/StringCounterTest.java
+++ src/test/java/com/ontariotechu/sofe3980U/StringCounterTest.java
@@ -1,8 +1,11 @@
 package com.ontariotechu.sofe3980U;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.ontariotechu.sofe3980U.StringCounter;
 
@@ -10,59 +13,110 @@ public class StringCounterTest {
     
     private StringCounter strcount;
 
-    @BeforeAll
+    @BeforeEach
     public void startTests()
     {
-        strcount = new StringCounterTest();
+        strcount = new StringCounter();
     }
 
-    @Test
+    //==========================BLACK BOX============================//
+
+    //array
+    @Test @Tag("blackbox") @Tag("array")
     public void arrayNoMatch()
     {
         char[] arr = {'a', 'b', 'c'};
         assertEquals( strcount.countOccurances(arr, 'd'), 0);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("array")
     public void arrayOneMatch()
     {
         char[] arr = {'a', 'b', 'c'};
         assertEquals( strcount.countOccurances(arr, 'a'), 1);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("array")
     public void arrayManyMatch()
     {
         char[] arr = {'a', 'a', 'a'};
         assertEquals( strcount.countOccurances(arr, 'a'), 3);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("array")
     public void arrayEmpty()
     {
         char[] arr = {};
         assertEquals( strcount.countOccurances(arr, 'a'), 0);
     }
 
-    @Test
+    //string
+    @Test @Tag("blackbox") @Tag("string")
     public void stringNoMatch()
     {
         String str = "test";
         assertEquals( strcount.countOccurances(str, 'd'), 0);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("string")
     public void stringOneMatch()
     {
         String str = "test";
         assertEquals( strcount.countOccurances(str, 'e'), 1);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("string")
     public void stringManyMatch()
     {
         String str = "test";
         assertEquals( strcount.countOccurances(str, 't'), 2);
     }
-    @Test
+    @Test @Tag("blackbox") @Tag("string")
     public void stringEmpty()
     {
         String str = "";
         assertEquals( strcount.countOccurances(str, 'd'), 0);
     }
+
+    //==========================WHITE BOX============================//
+
+    //array
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"bb", "b", "bx"})
+    public void arrayWBNoMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s.toCharArray(), 'a' ), 0 );
+    }
+
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"a", "ab", "a", "ax"})
+    public void arrayWBOneMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s.toCharArray(), 'a' ), 1 );
+    }
+
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"aa"})
+    public void arrayWBTwoMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s.toCharArray(), 'a' ), 2 );
+    }
+
+    //string
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"bb", "b", "bx"})
+    public void stringWBNoMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s, 'a' ), 0 );
+    }
+
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"a", "ab", "a", "ax"})
+    public void stringWBOneMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s, 'a' ), 1 );
+    }
+
+    @ParameterizedTest @Tag("whitebox") @Tag("array")
+    @ValueSource(strings = {"aa"})
+    public void stringWBTwoMatch(String s)
+    {
+        assertEquals( strcount.countOccurances( s, 'a' ), 2 );
+    }
+
 }
